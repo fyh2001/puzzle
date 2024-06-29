df2cfc6032619ac44a665a1285221cf9662c6e83
fyh1215 <325936216@qq.com>
2024-06-29 19:37:31 +0800

    新增 用户鉴权

---
+++ server/app/middlewares/jwt/jwt.go
@@ -0,0 +1,44 @@
+package jwt
+
+import (
+	jwtUtil "puzzle/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+func JWT() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// TODO
+		var code int
+		var data interface{}
+
+		code = 200
+		token := c.GetHeader("Authorization")
+		if token == "" {
+			code = 40001 // 无token，无权限访问
+		} else {
+			// 解析token
+			claims, err := jwtUtil.ParseToken(token)
+			if err != nil {
+				code = 40002 // token不合法
+			} else {
+				c.Set("userId", claims.Id)
+				c.Set("username", claims.Username)
+			}
+			//else if time.Now().Unix() > claims.ExpiresAt {
+			//	code = result.TokenExpired.Code // token已过期
+			//}
+
+		}
+		if code != 200 {
+			c.JSON(200, gin.H{
+				"code": code,
+				"msg":  "无权限访问",
+				"data": data,
+			})
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
+}
